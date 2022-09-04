3e71c7dd396824055e4262d42c18bc975906a5e1
Nitro <eliasmaidanik@gmail.com>
2022-09-04 12:06:52 +0200

    Big text now supports numbers

--- commands/big-text.js
+++ commands/big-text.js
@@ -1,8 +1,14 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { numberToKeykap } = require('../tools/utils.js');
 const letterRegex = /[a-z]/i;
+const numberRegex = /[0-9]/i;
 
 function isLetter(str) {
 	return str.length === 1 && str.match(letterRegex);
+}
+
+function isNumber(str) {
+	return str.length === 1 && str.match(numberRegex);
 }
 
 module.exports = {
@@ -16,10 +22,13 @@ module.exports = {
 		let output = ':arrow_right:  ';
 		for (let i = 0; i < content.length; ++i) {
 			if (isLetter(content[i])) {
-				output += ':regional_indicator_' + content[i] + ': ';
+				output += `:regional_indicator_${content[i]}: `;
 			}
 			else if (content[i] == ' ') {
 				output += '   ';
+			}
+			else if (isNumber(content[i])) {
+				output += `${numberToKeykap(parseInt(content[i]))} `;
 			}
 		}
 		output += ' :arrow_left:';
